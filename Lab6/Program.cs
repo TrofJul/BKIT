e39d5df707b9b08867bd71987d89ea3867fd3b97
TrofJul <yulechka_t99@mail.ru>
2019-09-24 19:08:20 +0300

    Add files via upload

---
+++ Lab6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    delegate string MyDeleg(string p1, int p2);
+
+    class Program
+    {
+        static void TestFunction(string desription, string p1, int p2, MyDeleg func)
+        {
+            Console.Write(desription + ": ");
+            Console.WriteLine(func(p1, p2));
+        }
+
+        static void TestFunction1(string desription, string p1, int p2, Func<string, int, string> func)
+        {
+            Console.Write(desription + ": ");
+            Console.WriteLine(func(p1, p2));
+        }
+
+        //метод, соответствующий делегату MyDeleg: возвращает строку, состоящую из строки p1, повторенной p2 раз
+        static string MyDelegFunc(string p1, int p2)
+        {
+            string str = "";
+            for (int i = 0; i < p2; ++i)
+            {
+                str += p1;
+            }
+
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            string p1 = "Hello";
+            int p2 = 3;
+
+            Console.WriteLine("Делегат MyDeleg:");
+
+            TestFunction("Передача функции", p1, p2, MyDelegFunc);
+            TestFunction("Передача лямбда-выражения", p1, p2, (x, y) => x + y.ToString());
+
+            Console.WriteLine("\nОбобщенный делегат:");
+
+            TestFunction1("Передача функции", p1, p2, MyDelegFunc);
+            TestFunction1("Передача лямбда-выражения", p1, p2, (x, y) => x + y.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6_2/Lab6_2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    [AttributeUsage(AttributeTargets.Property,
+        AllowMultiple = false,
+        Inherited = false)]
+
+    public class NewAttribute : Attribute
+    {
+        public NewAttribute() { }
+
+        public NewAttribute(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+
+        public string Description { get; set; }
+    }
+    public class ClassForInspection
+    {
+        public ClassForInspection() { }
+        public ClassForInspection(string str) { }
+        public ClassForInspection(double d) { }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        [NewAttribute("Описание первого свойства")]
+        public int prop1 { get; set; }
+
+        [NewAttribute("Описание второго свойства")]
+        public int prop2 { get; set; }
+
+        public int prop3 { get; set; }
+        public int[] data;
+    }
+
+    class Program
+    {
+        public static bool GetPropertyAttribute(PropertyInfo checkType,
+            Type attributeType, out object attribute)
+        {
+            bool Result = false;
+            attribute = null;
+
+            var isAttribute = checkType.GetCustomAttributes(attributeType, false);
+            if (isAttribute.Length > 0)
+            {
+                Result = true;
+                attribute = isAttribute[0];
+            }
+
+            return Result;
+        }
+
+        static void Main(string[] args)
+        {
+            Type t = typeof(ClassForInspection);
+
+            Console.WriteLine("\nИнформация о типе:");
+            Console.WriteLine("Тип " + t.FullName + " унаследован от " + t.BaseType.FullName);
+
+            Console.WriteLine("Пространство имен " + t.Namespace);
+
+            Console.WriteLine("Находится в сборке " + t.AssemblyQualifiedName);
+
+            Console.WriteLine("\nКонструкторы:");
+            foreach (var x in t.GetConstructors())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nМетоды:");
+            foreach (var x in t.GetMethods())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nСвойства:");
+            foreach (var x in t.GetProperties())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nПоля данных (public):");
+            foreach (var x in t.GetFields())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nСвойства, помеченные атрибутом:");
+            foreach (var x in t.GetProperties())
+            {
+                object attrObj;
+                if (GetPropertyAttribute(x, typeof(NewAttribute), out attrObj))
+                {
+                    NewAttribute attr = attrObj as NewAttribute;
+                    Console.WriteLine(x.Name + " - " + attr.Description);
+                }
+            }
+
+            Console.WriteLine("\nВызов метода с помощью рефлексии:");
+
+            ClassForInspection cfi = new ClassForInspection();
+            object[] parameters = new object[] { 3.0, 2.0 };
+
+            object result = t.InvokeMember("Divide", BindingFlags.InvokeMethod, null, cfi, parameters);
+            Console.WriteLine($"Divide(3.0, 2.0) = {result}");
+            Console.ReadKey();
+
+        }
+    }
+}
